fae59e7296f9efd6f633e9e84599fd228954a55b
OskarAgnarsson <osa16@hi.is>
2023-03-09 14:13:56 +0000

    login button goes straight to project list

--- app/src/main/java/is/hi/hbv601g/projectplanner/LoginActivity.kt
+++ app/src/main/java/is/hi/hbv601g/projectplanner/LoginActivity.kt
@@ -1,8 +1,11 @@
 package `is`.hi.hbv601g.projectplanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
 
@@ -10,6 +13,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.d("TAG","onCreate: ")
+
+        val mLoginButton = findViewById<Button>(R.id.login)
+        mLoginButton.isEnabled = true
+        mLoginButton.isClickable = true
+        mLoginButton.setOnClickListener {
+            val intent = Intent(this, ProjectActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
