696a8e52103dcf96dd552ee5989ad47e7134e4a0
Ísak <isl5@hi.is>
2023-03-16 15:48:40 +0000

    micro adj. layout
    micro adjustment á layout fyrir fimmtudagsfundinn.

--- app/src/main/java/is/hi/hbv601g/projectplanner/data/DummyData.kt
+++ app/src/main/java/is/hi/hbv601g/projectplanner/data/DummyData.kt
@@ -2,7 +2,7 @@ package `is`.hi.hbv601g.projectplanner.data
 
 fun loadProjects(): List<Project> {
     return listOf<Project>(
-        Project(1,1,"Ameno", "Verkefnislýsing :)"),
+        Project(1,1,"Ameno", "Undirbúa það sem á að sýna á brakandi ferskum fimmtudagsfundi!"),
         Project(2,1,"Omenare", "Verkefnislýsing :)"),
         Project(3,1,"Imperavi", "Verkefnislýsing :)"),
         Project(4,1,"Ameno", "Verkefnislýsing :)"),
@@ -22,18 +22,20 @@ fun loadProjects(): List<Project> {
 
 fun loadTasks(): List<Task> {
     return listOf<Task> (
-        Task(1,1,"Ameno 1"),
-        Task(2,1,"Ameno 2"),
-        Task(3,1,"Ameno 3"),
-        Task(4,1,"Ameno 4"),
-        Task(5,1,"Ameno 5"),
-        Task(6,2,"Omenare 1")
+        Task(1,1,"Layouts"),
+        Task(2,1,"Sideswiping"),
+        Task(3,1,"Task"),
+        Task(4,1,"Ameno"),
+        Task(5,1,"Omenare"),
+        Task(6,2,"Ameno")
     )
 }
 
+
+
 fun loadGroupMembers(): List<GroupMembers> {
     return listOf<GroupMembers> (
-        GroupMembers(1,"Óskar the Great","osa16@hi.is", 1),
+        GroupMembers(1,"Óskar the Magnificent","osa16@hi.is", 1),
         GroupMembers(2,"Ísak CringeLORD", "isl5@hi.is", 1),
         GroupMembers(3,"Vigdís Bossman", "vigdis@hi.is", 1),
     )
